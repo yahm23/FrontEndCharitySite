750659ef2c6199a2e0e9392dc1de69998db449aa
Yousef Ahmed <yahmed1996a@gmail.com>
2020-05-11 14:33:56 +0100

    added banner, restructured blog page a bit

--- src/components/Pages/Blogs/index.js
+++ src/components/Pages/Blogs/index.js
@@ -43,8 +43,16 @@ render() {
     <div>
 
         <div className="blogBanner">
-          <div className="blogcontainer">         
-          <h1 className="Posts">Posts</h1>
+          <div className="blogcontainer">  
+
+          <div className="Posts" >
+            <h1>Posts</h1>
+            <div className= "wave-container">
+                <svg  className="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 130 1420 95">
+                <path fill="white" fillOpacity="1" d="M0,160L120,165.3C240,171,480,181,720,176C960,171,1200,149,1320,138.7L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+                </svg>
+            </div>
+          </div>
           <div className="">
             
           </div>
