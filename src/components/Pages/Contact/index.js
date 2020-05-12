835266122ff564c0a70a17e7a5db05d9db113f04
Yousef Ahmed <yahmed1996a@gmail.com>
2020-05-12 14:14:18 +0100

    made skeleton of contact page

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import Wave from '../../StructuralComponents/Wave'
+import BottomBanner from '../../StructuralComponents/BottomBanner'
+import { useMediaQuery } from 'react-responsive';
+
 
 const Contact=()=>{
-
-
+    let isMobile = useMediaQuery({ maxWidth: 767 })
 
     return (
+        <div>
+            <div className={isMobile? "contactContainer-mobile":"contactContainer"} >
 
-        <div className="contactContainer">
+                <div className="Posts" >
+                    <h1>Contact</h1>
+                    <Wave></Wave>
+                </div>
 
-            <div className="Posts" >
-            <h1>Contact</h1>
-            <Wave></Wave>
-
-          </div>
-          
-
+                <div className="contactContent">
+                    
+                </div>
+            
+            </div>
+                <BottomBanner></BottomBanner>
         </div>
     )
 }
