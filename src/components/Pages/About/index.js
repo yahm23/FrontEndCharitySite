c8f327b6bd1cc944b15e583a51d0c4bae75d06d0
Yousef Ahmed <yahmed1996a@gmail.com>
2020-05-12 11:10:49 +0100

    added redirect file and fixed some css

--- src/components/Pages/About/index.js
+++ src/components/Pages/About/index.js
@@ -7,7 +7,7 @@ export class About extends Component {
             <div>
                 
                 <h1 className="NavBarPadding"> About Content</h1>
-                <p>Bacon ipsum dolor amet swine pig chicken, prosciutto jerky alcatra biltong t-bone corned beef tenderloin beef ribs flank jowl bresaola shoulder. Venison cow flank, chislic ribeye boudin pork pork chop chuck tenderloin doner buffalo bacon. Alcatra biltong ribeye, turducken leberkas andouille picanha doner kevin. Fatback jerky short ribs, ribeye sirloin bacon cow landjaeger swine. Tenderloin shankle t-bone, bacon pork belly kielbasa ribeye buffalo sausage bresaola. Tri-tip pork belly prosciutto chislic swine. Porchetta tail leberkas ham hock pork loin landjaeger. Salami ground round hamburger, swine turkey shankle landjaeger pig tenderloin. Ground round venison shankle chuck flank pork loin. Cow drumstick ham flank jerky bresaola shankle jowl swine brisket. Shoulder chicken landjaeger, leberkas corned beef salami bresaola turducken. Pork pancetta leberkas, tongue chicken t-bone shankle swine frankfurter meatball porchetta ground round bresaola chuck. Shank corned beef tail, prosciutto chicken venison beef ribs meatloaf cupim pancetta t-bone. Pancetta drumstick tenderloin pork loin jowl capicola sausage bacon beef ribs ground round leberkas jerky. Beef ribs shoulder swine bacon shank salami, brisket chuck tail drumstick hamburger. Porchetta tenderloin pig shoulder cow beef, chislic salami. Beef ribs fatback frankfurter, chuck sirloin ball tip ribeye porchetta t-bone pancetta.</p>
+                <p></p>
                 <BottomBanner></BottomBanner>
             </div>
         )
