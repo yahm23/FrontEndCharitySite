87522cd2a522846c236b74f72b8a09a550dfe3ca
yahm23 <yahmed1996a@gmail.com>
2020-05-20 16:19:02 +0100

    removed submit logic for custom donation entries

--- src/components/Pages/Donation/form.js
+++ src/components/Pages/Donation/form.js
@@ -34,7 +34,7 @@ constructor(props) {
     }
 
     handleSubmit(event) {
-        if(this.state.amount!=''&& this.state.amount!='other'){
+        if(this.state.amount!=''){
         const postURL = "https://blog-back-end-green.herokuapp.com/donations"
         fetch(postURL, {
         method: 'POST',
