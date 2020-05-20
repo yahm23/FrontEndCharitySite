ca9e312cae8d8813414c041a039c4a1d73a5902a
yahm23 <yahmed1996a@gmail.com>
2020-05-20 12:26:08 +0100

    donate button made wider and with set px length

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   
   //Checking platform the page is loaded on; phone/smaller screen or a Desktop
   const DesktopOrMobile = () => {
-    let isMobile = useMediaQuery({ maxWidth: 787 })
+    let isMobile = useMediaQuery({ maxWidth: 970 })
     return (
  
       <div className ={`container${isMobile ? "-mobile" : ""}`}>
