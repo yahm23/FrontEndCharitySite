756922515b9dbc711d131da0da2d49a10a09d098
yahm23 <yahmed1996a@gmail.com>
2020-06-09 17:04:38 +0100

    made mailing list require the type of text as email

--- src/components/StructuralComponents/MailingList/index.js
+++ src/components/StructuralComponents/MailingList/index.js
@@ -71,7 +71,7 @@ const MailingList=()=> {
                 
                 <form className="email" onSubmit={handleSubmit}>
                     
-                    <input value={email} onChange={handleEmailChange} height="100px" id="email" placeholder="Email address" required></input>
+                    <input value={email} onChange={handleEmailChange} height="100px" id="email" placeholder="Email address" required type="email"></input>
                     <button type="submit"  className="chevronButton"><svg  height="32" fill="#B2B3B6"id="chevron-email" viewBox="0 0 32 32" width="32" xmlns="http://www.w3.org/2000/svg"><path d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"/></svg></button>
                 </form>
 
