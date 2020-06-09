6add587c512a151a981f1a57b843c36fd7e79ae0
yahm23 <yahmed1996a@gmail.com>
2020-06-09 11:02:50 +0100

    changed contact page  to useState, refactored it to use hooks

--- src/components/Pages/Contact/form.js
+++ src/components/Pages/Contact/form.js
@@ -1,40 +1,29 @@
-import React, { PureComponent } from 'react'
+import React, { useState } from 'react'
 import backendURL from '../../backendURLs.js'
 
 
-class Form extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state ={
-            name:'',
-            email:'',
-            number:'',
-            message:'',
-            
-        };
-    
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handlePhoneChange = this.handlePhoneChange.bind(this);
-        this.handleMessageChange = this.handleMessageChange.bind(this);
-        
-        this.handleSubmit = this.handleSubmit.bind(this);
+const Form = (props)=> {
+    const [name,setName]=useState();
+    const [email,setEmail]=useState();
+    const [number,setNumber]=useState();
+    const [message,setMessage]=useState();
+
+   
+
+    const handleNameChange=(event)=> {
+        setName(event.target.value);
+     }
+     const handleEmailChange=(event)=> {
+        setEmail(event.target.value);
+     }
+     const handlePhoneChange=(event)=> {
+         setNumber(event.target.value)
+     }
+    const handleMessageChange=(event)=> {
+       setMessage(event.target.value);
     }
 
-    handleNameChange(event) {
-        this.setState({name: event.target.value});
-    }
-    handleEmailChange(event) {
-        this.setState({email: event.target.value});
-    }
-    handlePhoneChange(event) {
-        this.setState({number: event.target.value});
-    }
-    handleMessageChange(event) {
-        this.setState({message: event.target.value});
-    }
-
-    handleSubmit(event) {
+    const handleSubmit=(event)=> {
         
         const postURL = `${backendURL}/contact-submissions`
         fetch(postURL, {
@@ -44,10 +33,10 @@ class Form extends PureComponent {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                name:this.state.name,
-                email:this.state.email,
-                number:this.state.number,
-                message:this.state.message
+                name:name,
+                email:email,
+                number:number,
+                message:message
             })
         })
         .then(            
@@ -57,43 +46,41 @@ class Form extends PureComponent {
     }
     
 
-    
-    render(){
     return (
-        <div className={this.props.isMobile? "card-mobile":"card"} id="form">
+        <div className={props.isMobile? "card-mobile":"card"} id="form">
             
                 <div className="message">
         
                     <h2>Send us a message</h2>
                 </div>
 
-                <form className="formComponents" onSubmit={this.handleSubmit} >
+                <form className="formComponents" onSubmit={handleSubmit} >
                 
                 <div  >
 
                     <div className="space">
                         <label htmlFor="name">Name</label>
-                        <input value={this.state.name} onChange={this.handleNameChange} type="text" id="name" name="user_name" required></input>
+                        <input value={name} onChange={handleNameChange} type="text" id="name" name="user_name" required></input>
                     </div>
                 
                 
                     <div className="space">
                         <label htmlFor="mail">E-mail</label>
-                        <input value={this.state.email} onChange={this.handleEmailChange} type="email" id="mail" name="user_email" required></input>
+                        <input value={email} onChange={handleEmailChange} type="email" id="mail" name="user_email" required></input>
                     </div>    
                     
                    
                 
                     <div className="space">
                         <label htmlFor="number">Phone Number(Optional)</label>
-                        <input value={this.state.number} onChange={this.handlePhoneChange} type="text" id="number" name="user_enumber" ></input>
+                        <input value={number} onChange={handlePhoneChange} type="text" id="number" name="user_enumber" ></input>
                     </div>
                     
                   
                 
                     <div className="space">
                         <label htmlFor="msg">Message</label>
-                        <textarea  rows="1" cols="33" value={this.state.message} onChange={this.handleMessageChange} id="msg" name="user_message" required></textarea>
+                        <textarea  rows="1" cols="33" value={message} onChange={handleMessageChange} id="msg" name="user_message" required></textarea>
                     </div>
                     
                     <button id= "submit" type="submit">Submit</button>
@@ -105,7 +92,7 @@ class Form extends PureComponent {
 
             </div>
                 
-    )}
+    )
 }
 
 export default Form
