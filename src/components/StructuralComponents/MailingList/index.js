593b9e108bceee2b63bc9266e59565a07e00df65
yahm23 <yahmed1996a@gmail.com>
2020-06-09 17:03:21 +0100

    alert for mailing list updated to be modal instead

--- src/components/StructuralComponents/MailingList/index.js
+++ src/components/StructuralComponents/MailingList/index.js
@@ -1,23 +1,51 @@
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import backendURL from '../../backendURLs.js'
+// import { useMediaQuery } from 'react-responsive';
 
-export class MailingList extends Component {
+import Modal from 'react-bootstrap/Modal'
+import ModalTitle from 'react-bootstrap/ModalTitle'
+import ModalHeader from 'react-bootstrap/ModalHeader'
+import ModalBody from 'react-bootstrap/ModalBody'
+import ModalFooter from 'react-bootstrap/ModalFooter'
+import Button from 'react-bootstrap/Button'
 
-    constructor(props) {
-        super(props);
-        this.state ={
-            email:''       
-        };
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+const MailingList=()=> {
+    const [email,setEmail]=useState();
+
+    const handleEmailChange= (event)=> {
+        setEmail(event.target.value);
     }
+    const [show, setShow] = useState(false);
+  
+  const handleClose = () => {setShow(false); window.location.reload()};
+  const handleShow = () => setShow(true)
 
-    handleEmailChange(event) {
-        this.setState({email: event.target.value});
-    }
+  const PopUp =()=>{
+    return(
+        <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <ModalHeader closeButton>
+          <ModalTitle>Email Submitted</ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+         You have been successfully added to our mailing list!
+        </ModalBody>
+        <ModalFooter>
+          <Button variant="secondary" onClick={handleClose}>
+            Ok
+          </Button>
+          {/* <Button variant="primary">Ok</Button> */}
+        </ModalFooter>
+      </Modal> 
+    )
+}
 
-    handleSubmit(event) {
+    const handleSubmit=(event)=> {
         const postURL = `${backendURL}/mailing-lists/`
         fetch(postURL, {
             method: 'POST',
@@ -26,34 +54,31 @@ export class MailingList extends Component {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                email:this.state.email
+                email:email
             })
         })
-        .then(()=>{
-            
-            alert('Your email has been submitted');
-        })
+        .then(handleShow())
         
 
-        // event.preventDefault();
+        event.preventDefault();
     }
 
-    render() {
+
         return (
             <div className="update">
-
+                <PopUp></PopUp>
                 <p className="keepUpdated" >Keep updated with our progress.</p>
                 
-                <form className="email" onSubmit={this.handleSubmit}>
+                <form className="email" onSubmit={handleSubmit}>
                     
-                    <input value={this.state.email} onChange={this.handleEmailChange} height="100px" id="email" placeholder="Email address" required></input>
+                    <input value={email} onChange={handleEmailChange} height="100px" id="email" placeholder="Email address" required></input>
                     <button type="submit"  className="chevronButton"><svg  height="32" fill="#B2B3B6"id="chevron-email" viewBox="0 0 32 32" width="32" xmlns="http://www.w3.org/2000/svg"><path d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"/></svg></button>
                 </form>
 
 
             </div>
         )
-    }
+    
 }
 
 export default MailingList
